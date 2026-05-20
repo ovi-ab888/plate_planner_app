fe2ed49ea6df1676de27a4f152e80766f7002f88
ovi-ab888 <oviabdulohab@gmail.com>
2026-05-20 08:19:37 +0600

    Update app.py

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ def check_password():
         .password-container {
             max-width: 450px;
             margin: 50px auto 0 auto;
-            padding: 3.5rem;
+            padding: 5.5rem;
             background: rgba(255,255,255,0.05);
             backdrop-filter: blur(20px);
             border-radius: 32px;
