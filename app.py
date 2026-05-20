b39622b1ee97abeab489ffada48720f3fdb05ac0
ovi-ab888 <oviabdulohab@gmail.com>
2026-05-20 08:30:46 +0600

    Update app.py

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ def check_password():
         
         /* Password Container with Glow Effect */
         .password-container {
-            max-width: 550px;
+            max-width: 450px;
             margin: 50px auto 0 auto;
             padding: 5.5rem;
             background: rgba(255,255,255,0.05);
